2e77d34d5ae5eb7080385ef2f6225b537e5fb027
Hang Nguyen <49593977+fallingstar3107@users.noreply.github.com>
2020-07-19 21:46:34 +0100

    Fixed error for while loop
    Fixed the error "comparison is possible only for atomic and list types"
    Replaced choice with choice() in the if loop conditions.
    Remove while loop.

--- main.R
+++ main.R
@@ -18,40 +18,28 @@ library(readxl)
 
 main <- function() 
 {
-  choice <- function()
-  {
-    # ask user for their choice
-    choice <- readline(prompt="Choose option 1 for Boston dataset, option 2 for Default dataset, option 3 to quit (1/2/3): ")
-    # convert character into integer
+  choice <- function(){
+    choice <- readline("Choose option 1 for Boston dataset, option 2 for Default dataset (1/2): ")
     choice <- as.integer(choice)
     return(choice)
   }
+  choice()
   
-  while (choice != 3) 
-  {
-    if (choice == 1) # Boston dataset
-    {
-      # Import the MASS library
-      install.packages(MASS)
-      library(MASS)
-      
-      # Import the Boston dataset
-      dataset <- attach(Boston)
-    }
-    
-    else if (choice == 2) # Default dataset
-    {
-      url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls"
-      GET(url, write_disk("default.xls", overwrite=TRUE))
-      dataset <- read_xls('default.xls', sheet = 1, skip = 1)
-    } 
-    
-    else # Wrong choice
-    {
-      print("Wrong choice. Please try again.")
-    }
+  if (choice() == 1) {
+    install.packages(MASS) # Import the MASS library
+    library(MASS)
+    dataset <- attach(Boston) # Import the Boston dataset
   }
-   
+  
+  else if (choice() == 2) { 
+    url <- "https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls"
+    GET(url, write_disk("default.xls", overwrite=TRUE))
+    dataset <- read_xls('default.xls', sheet = 1, skip = 1)
+  } 
+  
+  else {
+    break
+  }
 }
 
 if(!interactive()) {
