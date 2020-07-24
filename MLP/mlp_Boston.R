c31a1e8cee908300d08540c37ebeacf2961352b1
Hang Nguyen <49593977+fallingstar3107@users.noreply.github.com>
2020-07-24 14:24:35 +0100

    Update old NN for Boston dataset using neuralnet

--- MLP/mlp_Boston.R
+++ MLP/mlp_Boston.R
@@ -1,6 +1,6 @@
 ------------------------------------------------------------------------
 ## 1. PACKAGES INSTALLATION AND ACTIVATION 
-
+  
 #install.packages(keras)
 #install.packages(tensorflow)
 #install.packages(MASS)
@@ -38,25 +38,43 @@ test_scaled_NN = scaled[-index , ]
 ------------------------------------------------------------------------
 ## 5. TRAIN NN MODEL
 set.seed(2)
-NN = neuralnet(crim ~ zn + indus + chas + nox + rm + age + dis + rad + tax + ptratio + black + lstat + medv, train_scaled_NN, hidden = 5 , linear.output = TRUE, threshold=0.01 )
+NN = neuralnet(crim ~ zn + indus + chas + nox + rm + age  + dis + rad + tax + 
+                 ptratio + black + lstat + medv, train_scaled_NN, hidden = 5 ,
+               linear.output = TRUE)
 NN$result.matrix
 plot(NN)
 
 ------------------------------------------------------------------------
 ## 6. TEST NN MODEL
 predict_test_scaled_NN = compute(NN, test_scaled_NN[,c(2:14)])
-predict_test_scaled_NN <- data.frame(actual = test_scaled_NN$crim, prediction = predict_test_scaled_NN$net.result)
+predict_test_scaled_NN <- data.frame(actual = test_scaled_NN$crim, 
+                                prediction = predict_test_scaled_NN$net.result)
 
 ------------------------------------------------------------------------
 ## 7. MODEL ACCURACY
 # Revert scaled data back to originally formatted data  
 predict_test_NN=predict_test_scaled_NN$prediction * abs(diff(range(crim))) + min(crim)
 actual_test_NN=predict_test_scaled_NN$actual * abs(diff(range(crim))) + min(crim)
- 
+
 # Regression: RMSE and R^2 for the test set
 RMSE = ( sum((actual_test_NN-predict_test_NN)^2) / nrow(testNN) )^ 0.5
-R_squared = 1 - sum((actual_test_NN-predict_test_NN)^2)/sum((actual_test_NN - mean(actual_test_NN))^2)
+R_squared = 1 - 
+  sum((actual_test_NN-predict_test_NN)^2)/sum((actual_test_NN - mean(actual_test_NN))^2)
 print('RMSE = ')  
 RMSE
 print('R squared = ')  
 R_squared
+
+# Create the data arguments for glmnet
+x = model.matrix (crim ~., Boston) [,1]
+y = Boston$crim
+dim(x)
+dim(y)
+
+# Ridge regression regularisation
+#install.packages(glmnet)
+library (glmnet)
+grid =10^ seq (10 , -2 , length =100)
+ridge.mod = glmnet(x, y, alpha = 0, lambda = grid)
+dim(coef(ridge.mod))
+
