d673d32eb6047d70b7775d2c475c485fc90c2849
Hang Nguyen <49593977+fallingstar3107@users.noreply.github.com>
2020-07-25 23:30:43 +0100

    Fixed bugs
    Update balancing data, reduce no. of layers, remove one-hot encoding

--- MLP/mlp_Default.R
+++ MLP/mlp_Default.R
@@ -9,6 +9,7 @@
 #install.packages(remotes)
 #remotes::install_github("rstudio/tensorflow")
 #install.packages(ggplot2)
+#install.packages(matrixStats)
 
 library(tensorflow)
 #install_tensorflow(version = "2.0.0")
@@ -18,6 +19,7 @@ library(httr)
 library(RCurl)
 library(readxl)
 library(ggplot2)
+library(matrixStats)
 
 #------------------------------------------------------------------------
 ## 2. DATA PREPROCESSING
@@ -44,69 +46,54 @@ Y_train = as.matrix(dataset[index , 24])
 X_test = as.matrix(dataset[-index , 1:23])
 Y_test = as.matrix(dataset[-index , 24])
 
-
 #------------------------------------------------------------------------
 ## 4. DATA SCALING 
 
 scaled_X_train = scale(X_train)
 scaled_X_test = scale(X_test)
 
-Y_train <- to_categorical(Y_train, 2)
-Y_test <- to_categorical(Y_test, 2)
-
 #------------------------------------------------------------------------
 ## 5. TRAIN NN MODEL
 # Construct the NN
 set.seed(80)
-mlp <- keras_model_sequential() %>% 
-  layer_dense(units = 16, activation = 'relu', input_shape = dim(X_train)[2]) %>% 
+mlp <- keras_model_sequential()  %>% 
+  layer_dense(units = 16, input_shape = dim(X_train)[2]) %>% 
   layer_dropout(rate = 0.5) %>% 
-  layer_dense(units = 8, activation = 'relu') %>%
-  layer_dropout(rate = 0.5) %>%
-  layer_dense(units = 4, activation = 'relu')
-  layer_dropout(rate = 0.5) %>%
+  layer_dense(units = 4, activation = 'relu')%>% 
+  layer_dropout(rate = 0.5) %>% 
   layer_dense(units = 1, activation = 'sigmoid')
 
 set.seed(80)
-mlp_l2 <- keras_model_sequential() %>% 
-  layer_dense(units = 16, activation = 'relu', input_shape = dim(X_train)[2], 
+mlp_l2 <- keras_model_sequential()  %>% 
+  layer_dense(units = 16, input_shape = dim(X_train)[2], 
               kernel_regularizer = regularizer_l2(l = 0.001)) %>% 
   layer_dropout(rate = 0.5) %>% 
-  layer_dense(units = 8, activation = 'relu', 
-              kernel_regularizer = regularizer_l2(l = 0.001)) %>%
-  layer_dropout(rate = 0.5) %>%
   layer_dense(units = 4, activation = 'relu', 
-              kernel_regularizer = regularizer_l2(l = 0.001))
-  layer_dropout(rate = 0.5) %>%
+              kernel_regularizer = regularizer_l2(l = 0.001))%>% 
+  layer_dropout(rate = 0.5) %>% 
   layer_dense(units = 1, activation = 'sigmoid')
   
 set.seed(80)
-mlp_l1 <- keras_model_sequential() %>% 
-  layer_dense(units = 16, activation = 'relu', input_shape = dim(X_train)[2], 
+mlp_l1 <- keras_model_sequential()  %>% 
+  layer_dense(units = 16, input_shape = dim(X_train)[2], 
               kernel_regularizer = regularizer_l1(l = 0.001)) %>% 
-  layer_dropout(rate = 0.5) %>% 
-  layer_dense(units = 8, activation = 'relu', 
-              kernel_regularizer = regularizer_l1(l = 0.001)) %>%
-  layer_dropout(rate = 0.5) %>%
+  layer_dropout(rate = 0.5)%>% 
   layer_dense(units = 4, activation = 'relu', 
-              kernel_regularizer = regularizer_l1(l = 0.001))
-  layer_dropout(rate = 0.5) %>%
+              kernel_regularizer = regularizer_l1(l = 0.001))%>% 
+  layer_dropout(rate = 0.5) %>% 
   layer_dense(units = 1, activation = 'sigmoid')
 
 set.seed(80)
-mlp_l1_l2 <- keras_model_sequential() %>% 
-  layer_dense(units = 16, activation = 'relu', input_shape = dim(X_train)[2], 
+mlp_l1_l2 <- keras_model_sequential()  %>% 
+  layer_dense(units = 16, input_shape = dim(X_train)[2], 
               kernel_regularizer = regularizer_l1_l2(l1 = 0.001, l2 = 0.001 )) %>% 
   layer_dropout(rate = 0.5) %>% 
-  layer_dense(units = 8, activation = 'relu', 
-              kernel_regularizer = regularizer_l1_l2(l1 = 0.001, l2 = 0.001)) %>%
-  layer_dropout(rate = 0.5) %>%
   layer_dense(units = 4, activation = 'relu', 
-              kernel_regularizer = regularizer_l1_l2(l1 = 0.001, l2 = 0.001))
-  layer_dropout(rate = 0.5) %>%
+              kernel_regularizer = regularizer_l1_l2(l1 = 0.001, l2 = 0.001))%>% 
+  layer_dropout(rate = 0.5) %>% 
   layer_dense(units = 1, activation = 'sigmoid')
 
-opt<-optimizer_adam( lr= 0.001 , decay = 0, clipnorm = 1 )
+opt<-optimizer_adam( lr= 0.0001 , decay = 0, clipnorm = 1 )
 mlp %>% 
   compile(loss = 'binary_crossentropy', optimizer = opt, metrics = c("accuracy"))
 mlp_l2 %>% 
@@ -128,50 +115,63 @@ print_dot_callback <- callback_lambda(
     cat(".")
   }
 )  
+early_stop <- callback_early_stopping(monitor = "val_loss", patience = 20)
+
+# set class weight for the imbalanced dataset
+set.seed(80)
+total = nrow(dataset)
+neg = binCounts(dataset$default, bx =c(0, 1))
+pos = total - neg
 visualisation <- mlp %>% 
   fit(X_train, Y_train,
-          batch_size = 32, epochs = 20,
-          verbose = 0, validation_split = 0.2,   callbacks = list(print_dot_callback))
-plot(visualisation)
+          batch_size = 128, epochs = 100,
+          verbose = 0, validation_split = 0.2,   
+          class_weight = list("0"=total/(2*neg),"1"=total/(2*pos)),
+          callbacks = list(early_stop, print_dot_callback))
+plot(visualisation, smooth = FALSE, main = "Original MultiLayer Perceptron", xlab = "Number of Iterations")
 
 visualisation_l2 <- mlp_l2 %>% 
   fit(X_train, Y_train,
-      batch_size = 32, epochs = 20,
-      verbose = 0, validation_split = 0.2,  callbacks = list(print_dot_callback))
-plot(visualisation_l2)
+      batch_size = 128, epochs = 100,
+      verbose = 0, validation_split = 0.2,  
+      class_weight = list("0"=total/(2*neg),"1"=total/(2*pos)),
+      callbacks = list(early_stop, print_dot_callback))
+plot(visualisation_l2, smooth = FALSE, main = "L2 Regularised MultiLayer Perceptron", xlab = "Number of Iterations") 
 
 visualisation_l1 <- mlp_l1 %>% 
   fit(X_train, Y_train,
-      batch_size = 32, epochs = 20,
-      verbose = 0, validation_split = 0.2,  callbacks = list(print_dot_callback))
-plot(visualisation_l1)
+      batch_size = 128, epochs = 100,
+      verbose = 0, validation_split = 0.2,  
+      class_weight = list("0"=total/(2*neg),"1"=total/(2*pos)),
+      callbacks = list(early_stop, print_dot_callback))
+plot(visualisation_l1, smooth = FALSE, main = "L1 Regularised MultiLayer Perceptron", xlab = "Number of Iterations")
 
 visualisation_l1_l2 <- mlp_l1_l2 %>% 
   fit(X_train, Y_train,
-      batch_size = 32, epochs = 20,
-      verbose = 0, validation_split = 0.2,  callbacks = list(print_dot_callback))
-plot(visualisation_l1_l2)
+      batch_size = 128, epochs = 100,
+      verbose = 0, validation_split = 0.2,  
+      class_weight = list("0"=total/(2*neg),"1"=total/(2*pos)),
+      callbacks = list(early_stop, print_dot_callback))
+plot(visualisation_l1_l2, smooth = FALSE, main = "L1 and L2 Regularised MultiLayer Perceptron", xlab = "Number of Iterations")
 
 #Evaluate the model
+set.seed(80)
 mlp %>% evaluate(X_test, Y_test, batch_size = 32)
-
-mlp_l1 %>% evaluate(X_test, Y_test, batch_size = 32)
-
 mlp_l2 %>% evaluate(X_test, Y_test, batch_size = 32)
-
+mlp_l1 %>% evaluate(X_test, Y_test, batch_size = 32)
 mlp_l1_l2 %>% evaluate(X_test, Y_test, batch_size = 32)
 
 #------------------------------------------------------------------------
 ## 6. TEST NN MODEL
-mlp %>% predict_classes(X_test)
-mlp_l2 %>% predict_classes(X_test)
-mlp_l1 %>% predict_classes(X_test)
-mlp_l1_l2 %>% predict_classes(X_test)
-
+set.seed(80)
+pred_mlp <- predict_classes(mlp, X_test)
+pred_mlp_l2 <- predict_classes(mlp_l2, X_test)
+pred_mlp_l1 <- predict_classes(mlp_l1, X_test)
+pred_mlp_l1_l2 <- predict_classes(mlp_l1_l2, X_test)
+df <- data.frame(pred_mlp,pred_mlp_l2,pred_mlp_l1,pred_mlp_l1_l2)
+summary(df)
 #------------------------------------------------------------------------
 ## 7. K-FOLD CROSS VALIDATION
 
 ## 8. MODEL ASSESSMENT: performance, accuracy, interpretability, efficiency.
-
-
 
