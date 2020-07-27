20f2617092ab86586b7fc85f096b238772bac9e0
Hang Nguyen <49593977+fallingstar3107@users.noreply.github.com>
2020-07-27 06:33:16 +0100

    Update visualisation

--- MLP/mlp_Boston.R
+++ MLP/mlp_Boston.R
@@ -1,26 +1,26 @@
-#------------------------------------------------------------------------
-## 1. PACKAGES INSTALLATION AND ACTIVATION 
-#------------------------------------------------------------------------
+# INSTALL/ACTIVATE PACKAGES -----------------------------------------------
 #install.packages(tensorflow)
 #install.packages(remotes)
 #remotes::install_github("rstudio/tensorflow")
 #install.packages(RSNNS)
+#install.packages(ggplot2)
 
 library(tensorflow) # build ANN model
 #install_tensorflow(version = "2.0.0")
 library(keras) # build ANN model
 library(RSNNS) # Data normalisation
-#------------------------------------------------------------------------
-## 2. DATA PREPROCESSING
-#------------------------------------------------------------------------
-# Remove missing values
+library(ggplot2)
+
+# LOAD DATA ---------------------------------------------------------------
 library(MASS)
 attach(Boston)
+
+# CLEAN DATA --------------------------------------------------------------
 dataset <- na.omit(Boston)
 with(Boston, sum(is.na(crim)))
-#------------------------------------------------------------------------
-## 3. DATA SPLIT: TRAIN DATASET AND TEST DATASET 
-#------------------------------------------------------------------------
+
+# TRAIN/TEST DATA SPLIT ---------------------------------------------------
+
 # Random sampling, create training (80%) and test set (20%)
 set.seed(1)
 samplesize = 0.80 * nrow(dataset)
@@ -30,19 +30,15 @@ Y_train = as.matrix(dataset[index , 1])
 X_test = as.matrix(dataset[-index , 2:14])
 Y_test = as.matrix(dataset[-index , 1])
 
-#------------------------------------------------------------------------
-## 4. DATA SCALING
-#------------------------------------------------------------------------
+# DATA SCALING ------------------------------------------------------------
 # Normalize data, range from 0 to 1
 scaled_X_train = normalizeData(X_train, type = "0_1")
 scaled_X_test = normalizeData(X_test, type = "0_1")
 scaled_Y_train = normalizeData(Y_train, type = "0_1")
 scaled_Y_test = normalizeData(Y_test, type = "0_1")
 
-#------------------------------------------------------------------------
-## 5. TRAIN NN MODEL
-#------------------------------------------------------------------------
-# Construct the NN
+# CONSTRUCT MODELS --------------------------------------------------------
+
 set.seed(1)
 mlp <- keras_model_sequential() %>%
   layer_dense(units = 8, input_shape = dim(X_train)[2], 
@@ -117,23 +113,32 @@ mlp_l1 %>% compile(
 mlp_l1_l2 %>% compile(
   loss = "mse", optimizer = opt,metrics = list("mean_squared_error"))
 
+# MODELS STRUCTURE SUMMARY ----------------------------------------------------
+
+
 mlp %>% summary()
 mlp_l2 %>% summary()
 mlp_l1 %>% summary()
 mlp_l1_l2 %>% summary()
 
-# Create k-fold cross validation
+# TRAIN/FIT HISTORIES
+# K-FOLD CROSS VALIDATION -------------------------------------------------
 k <- 5
 indices <- sample(1:nrow(scaled_X_train))
 folds <- cut(1:length(indices), breaks = k, labels = FALSE) 
 
-mlp_mse_histories <- NULL
-l2_mlp_mse_histories <- NULL
-l1_mlp_mse_histories <- NULL
-l1_l2_mlp_mse_histories <- NULL
+train_mse_histories_mlp <- NULL
+val_mse_histories_mlp <- NULL
+train_mse_histories_mlp_l2 <- NULL
+val_mse_histories_mlp_l2 <- NULL
+train_mse_histories_mlp_l1 <- NULL
+val_mse_histories_mlp_l1 <- NULL
+train_mse_histories_mlp_l1_l2 <- NULL
+val_mse_histories_mlp_l1_l2 <- NULL
 
 for (i in 1:k) {
   cat("processing fold #", i, "\n")
+  # TRAINING/VALIDATION DATA PARTITION ----------------------------------
   
   # Prepare the validation data: data from partition # k
   indices_val <- which(folds == i, arr.ind = TRUE) 
@@ -144,7 +149,9 @@ for (i in 1:k) {
   partial_scaled_X_train <- scaled_X_train[-indices_val,]
   partial_scaled_Y_train <- scaled_Y_train[-indices_val]
   
-  #Train the NN
+  # TRAIN/FIT MODELS  ----------------------------------------------------------
+  
+  # Create call backs
   set.seed(1)
   print_dot_callback <- callback_lambda(
     on_epoch_end = function(epoch, logs) {
@@ -152,8 +159,11 @@ for (i in 1:k) {
       cat(".")
     }
   )  
+  
+  # Create early stopping
   early_stop <- callback_early_stopping(monitor = "val_loss", patience = 50)
   
+  # Fit models MLP 
   set.seed(1)
   history_mlp <- mlp %>% fit(
     partial_scaled_X_train,
@@ -164,6 +174,7 @@ for (i in 1:k) {
     batch_size = 1,
     callbacks = list(early_stop, print_dot_callback))
   
+  # Fit models MLP, using L2 regularisation 
   set.seed(1)
   history_mlp_l2 <- mlp_l2 %>% fit(
     partial_scaled_X_train,
@@ -174,6 +185,7 @@ for (i in 1:k) {
     batch_size = 1,
     callbacks = list(early_stop, print_dot_callback))
   
+  # Fit models MLP, using L1 regularisation 
   set.seed(1)
   history_mlp_l1 <- mlp_l1 %>% fit(
     partial_scaled_X_train,
@@ -184,6 +196,7 @@ for (i in 1:k) {
     batch_size = 1,
     callbacks = list(early_stop, print_dot_callback))
   
+  # Fit models MLP, using L1 and L2 regularisation 
   set.seed(1)
   history_mlp_l1_l2 <- mlp_l1_l2 %>% fit(
     partial_scaled_X_train,
@@ -193,70 +206,86 @@ for (i in 1:k) {
     verbose = 0, 
     batch_size = 1,
     callbacks = list(early_stop, print_dot_callback))
+
+  # LOAD TRAIN/FIT HISTORIES -----------------------------------------------------
   
-  #------------------------------------------------------------------------
-  ## 6. TEST NN MODEL
-  #------------------------------------------------------------------------
-  set.seed(1)
-  scaled_Yhat_mlp <- predict(mlp, scaled_X_test)
-  Yhat_mlp = denormalizeData(scaled_Yhat_mlp, getNormParameters(scaled_Y_test))
+  add_train_mse_history_mlp <- history_mlp$loss
+  train_mse_histories_mlp <- rbind(train_mse_histories_mlp, 
+                                   add_train_mse_history_mlp)
+  add_val_mse_histories_mlp <- history_mlp$val_loss
+  val_mse_histories_mlp <- rbind(val_mse_histories_mlp, 
+                                 add_val_mse_histories_mlp)
   
-  set.seed(1)
-  scaled_Yhat_mlp_l2 <- predict(mlp_l2, scaled_X_test)
-  Yhat_mlp_l2 = denormalizeData(scaled_Yhat_mlp_l2, getNormParameters(scaled_Y_test))
+  add_train_mse_histories_mlp_l2 <- history_mlp_l2$loss
+  train_mse_histories_mlp_l2 <- rbind(train_mse_histories_mlp_l2, 
+                                      add_train_mse_histories_mlp_l2)
+  add_val_mse_histories_mlp_l2 <- history_mlp_l2$val_loss
+  val_mse_histories_mlp_l2 <- rbind(val_mse_histories_mlp_l2, 
+                                    add_val_mse_histories_mlp_l2)
   
-  set.seed(1)
-  scaled_Yhat_mlp_l1 <- predict(mlp_l1, scaled_X_test)
-  Yhat_mlp_l1 = denormalizeData(scaled_Yhat_mlp_l1, getNormParameters(scaled_Y_test))
+  add_train_mse_histories_mlp_l1 <- history_mlp_l1$loss
+  train_mse_histories_mlp_l1 <- rbind(train_mse_histories_mlp_l1, 
+                                      add_train_mse_histories_mlp_l1)
+  add_val_mse_histories_mlp_l1 <- history_mlp_l1$val_loss
+  val_mse_histories_mlp_l1 <- rbind(val_mse_histories_mlp_l1, 
+                                    add_val_mse_histories_mlp_l1)
   
-  set.seed(1)
-  scaled_Yhat_mlp_l1_l2 <- predict(mlp_l1_l2, scaled_X_test)
-  Yhat_mlp_l1_l2 = denormalizeData(scaled_Yhat_mlp_l1_l2, getNormParameters(scaled_Y_test))
+  add_train_mse_histories_mlp_l1_l2 <- history_mlp_l1_l2$loss
+  train_mse_histories_mlp_l1_l2 <- rbind(train_mse_histories_mlp_l1_l2, 
+                                         add_train_mse_histories_mlp_l1_l2)
+  add_val_mse_histories_mlp_l1_l2 <- history_mlp_l1_l2$val_loss
+  val_mse_histories_mlp_l1_l2 <- rbind(val_mse_histories_mlp_l1_l2, 
+                                       add_val_mse_histories_mlp_l1_l2)
   
-  df <- data.frame("Actual" = Y_test, "Baseline" = Yhat_mlp, "L2" = Yhat_mlp_l2, "L1" = Yhat_mlp_l1, "L1_L2" = Yhat_mlp_l1_l2)
-  
-  #Evaluate the model
-  MSE_mlp = mean((df$Actual - df$Baseline)^2)
-  MSE_mlp_l2 = mean((df$Actual - df$L2)^2)
-  MSE_mlp_l1 = mean((df$Actual - df$L1)^2)
-  MSE_mlp_l1_l2 = mean((df$Actual - df$L1_L2)^2)
-  
-  mlp_mse_histories <- rbind(mlp_mse_histories, MSE_mlp)
-  l2_mlp_mse_histories <- rbind(l2_mlp_mse_histories, MSE_mlp_l2)
-  l1_mlp_mse_histories <- rbind(l1_mlp_mse_histories, MSE_mlp_l1)
-  l1_l2_mlp_mse_histories <- rbind(l1_l2_mlp_mse_histories, MSE_mlp_l1_l2)
 }
+# MODELS COMPARISION -----------------------------------------------------------
+# Compute the average train/val MSE for all folds of 4 models
+d1 <- data.frame(
+  train_loss = apply(train_mse_histories_mlp, 2, mean), 
+  valid_loss = apply(val_mse_histories_mlp, 2, mean))
+plt1 <- plot_Keras_fit_trajectory(
+  d1,
+  title = "MLP performance by epoch")
+suppressWarnings(print(plt1)) # too few points for loess
 
+d2 <- data.frame(
+  train_loss = apply(train_mse_histories_mlp_l2, 2, mean), 
+  valid_loss = apply(val_mse_histories_mlp_l2, 2, mean))
+plt2 <- plot_Keras_fit_trajectory(
+  d2,
+  title = "MLP (L2 regularisation) performance by epoch")
+suppressWarnings(print(plt2)) # too few points for loess
 
-#------------------------------------------------------------------------
-## 8. MODEL ASSESSMENT: performance, accuracy, interpretability, efficiency.
-#------------------------------------------------------------------------
-# Compute the average of the per-epoch MSE scores for all folds
-average_mse_history_mlp <- data.frame(
-  epoch = seq(1:ncol(mlp_mse_histories)),
-  validation_mse = apply(mlp_mse_histories, 2, mean)
-)
-average_mse_history_mlp_l2 <- data.frame(
-  epoch = seq(1:ncol(l2_mlp_mse_histories)),
-  validation_mse = apply(l2_mlp_mse_histories, 2, mean)
-)
-average_mse_history_mlp_l1 <- data.frame(
-  epoch = seq(1:ncol(l1_mlp_mse_histories)),
-  validation_mse = apply(l1_mlp_mse_histories, 2, mean)
-)
-average_mse_history_mlp_l1_l2 <- data.frame(
-  epoch = seq(1:ncol(l1_l2_mlp_mse_histories)),
-  validation_mse = apply(l1_l2_mlp_mse_histories, 2, mean)
-)
+d3 <- data.frame(
+  train_loss = apply(train_mse_histories_mlp_l1, 2, mean), 
+  valid_loss = apply(val_mse_histories_mlp_l1, 2, mean))
+plt3 <- plot_Keras_fit_trajectory(
+  d3,
+  title = "MLP (L1 regularisation) performance by epoch")
+suppressWarnings(print(plt3)) # too few points for loess
 
+d4 <- data.frame(
+  train_loss = apply(train_mse_histories_mlp_l1_l2, 2, mean), 
+  valid_loss = apply(val_mse_histories_mlp_l1_l2, 2, mean))
+plt4 <- plot_Keras_fit_trajectory(
+  d4,
+  title = "MLP (L1 and L2 regularisation) performance by epoch")
+suppressWarnings(print(plt4)) # too few points for loess
 
-# Visualisation
-library(ggplot2)
-ggplot(average_mse_history_mlp, aes(x = epoch, y = validation_mse)) + geom_smooth()
-ggplot(average_mse_history_mlp_l2, aes(x = epoch, y = validation_mse)) + geom_smooth()
-ggplot(average_mse_history_mlp_l1, aes(x = epoch, y = validation_mse)) + geom_smooth()
-ggplot(average_mse_history_mlp_l1_l2, aes(x = epoch, y = validation_mse)) + geom_smooth()
+# TEST THE BEST MODEL -------------------------------------------------------------
+# Predict the normalised y
+set.seed(1)
+scaled_Y_hat <- predict(mlp, scaled_X_test)
 
+# Invert the normalised y back to the original scale
+Y_hat = denormalizeData(scaled_Y_hat, getNormParameters(scaled_Y_test))
 
+# MODEL RESULT ---------------------------------------------------------
+# Calculate the MSE
+MSE = mean((Y_test - Y_hat)^2)
 
+# Calculate the R^2
+R2 = 1 - sum((Y_test - Y_hat)^2)/sum((Y_test - mean(Y_test))^2)
+
+paste("MSE:", MSE, "R squared:", R2)
 detach(Boston)
